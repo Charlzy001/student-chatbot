a3325851690bde94a801ed7a3abc34c290325abb
Charlzy001 <charlesajisola@yahoo.com>
2024-10-13 08:33:06 -0700

    new update

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -7,7 +7,7 @@ import OpenAI from "openai";
 const runtime ="edge"
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(req: Request, res: NextApiResponse) {
